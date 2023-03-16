1c3b5e8a7505c55c2cc050a92db5cdcde6e45f45
Slava02 <67589455+Slava02@users.noreply.github.com>
2023-03-16 09:41:10 +0300

    Completed k-stat

--- k_stat/k_stat_cpp/main.cpp
+++ k_stat/k_stat_cpp/main.cpp
@@ -1,35 +1,83 @@
 #include <iostream>
+using namespace std;
 
-using std::swap;
-using std::cin;
-using std::cout;
+void swap(int *a, int firstIndex, int secondIndex) {
+    int temp = a[firstIndex];
+    a[firstIndex] = a[secondIndex];
+    a[secondIndex] = temp;
+}
 
-int GetOrderStat(int A[], int left, int right, int k);;
-
-int main(){
-    int n, k;
-    cin >> n >> k;
-    int * array = new int[n];
-    for(int i = 0; i < n; i++)
-        cin >> array[i];
-    cout << GetOrderStat(array, 0, n-1, k);
-    delete[] array;
-    return 0;
+int findPivot(int *a, int left, int right,
+              bool (*compareFunc)(const int &a, const int &b)) {
+    int middle = (left + right) / 2;
+    if (compareFunc(a[right], a[left])) {
+        swap(a, right, left);
+    }
+    if (compareFunc(a[right], a[middle])) {
+        swap(a, middle, right);
+    } else if (compareFunc(a[middle], a[left])) {
+        swap(a, middle, left);
+    }
+    return middle;
 }
 
 
-int GetOrderStat(int A[], int left, int right, int k) {
-    int i=left, j=right, x=A[left], num=0; 
-    if(left==right) return A[left];
-    do {
-        while(A[i] < x)i++; 
-        while(A[j]>x)j--; 
-        if(i <= j) {
-            swap(A[i], A[j]);
-            i++; 
-            j--; 
+int partition(int *a, int left, int right,
+              bool (*compareFunc)(const int &a, const int &b)) {
+    if (right - left < 1) {
+        return left;
+    }
+    int pivotPosition = findPivot(a, left, right, compareFunc);
+    swap(a, left, pivotPosition);
+    int i = right;
+    int j = i;
+    while (j > 0) {
+        if (compareFunc(a[left], a[j])) {
+            swap(a, i, j);
+            i--;
         }
-    } while(i<j);
-    if(k<= num) {return GetOrderStat(A, left, i-1, k);}
-    else return GetOrderStat(A, i, right, k-num);
+        j--;
+    }
+    swap(a, left, i);
+    return i;
+}
+
+bool defaultCompare(const int &a, const int &b) {
+    return a < b;
+}
+
+int findKStat(int *a, int n, int k,
+            bool (*compareFunc)(const int &a, const int &b) = defaultCompare) {
+    int left = 0;
+    int right = n - 1;
+    int part = partition(a, left, right, compareFunc);
+    while (part != k) {
+        if (k < part) {
+            right = part - 1;
+        } else {
+            left = part + 1;
+        }
+        part = partition(a, left, right, compareFunc);
+    }
+    return a[part];
+}
+
+
+int main() {
+    int n = 0;
+    int k = 0;
+    while(1) {
+        cout << " N: ";
+        cin >> n;
+        cout << "\n K: ";
+        cin >> k;
+        int *A = new int[n];
+        cout << "\n Enter: ";
+        for (int i = 0; i < n; ++i) {
+            cin >> A[i];
+        }
+        cout << "\n K-stat (from zero): " << findKStat(A, n, k) << endl;
+        delete[] A;
+    }
+    return 0;
 }
